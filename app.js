c5971bdad22287cebe35d42ec9bd8f411664aaa9
Bacco40 <bacchettimatteo@gmail.com>
2022-06-25 19:47:36 +0200

    edited axios link

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 app.use('/', indexRouter);
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
