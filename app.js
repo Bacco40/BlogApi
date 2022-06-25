7e1c3d2ed92c06ce7e6f9d132f3a9f52726063f9
Bacco40 <bacchettimatteo@gmail.com>
2022-06-25 17:37:11 +0200

    edited path

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/src', 'index.js'));
+    res.sendFile(path.join(__dirname, './client/public', 'index.html'));
   });
 
 app.use('/', indexRouter);
